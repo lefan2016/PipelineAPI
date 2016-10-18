069799cb8e07ace8c9bd937b7a1e8f9e2d217f9e
GuenMo Kim <hybrid0815@gmail.com>
2016-10-18 17:07:53 +0900

    [mod] 문서화 준비

--- PipelineAPI/docs/conf.py
+++ PipelineAPI/docs/conf.py
@@ -21,6 +21,35 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 from PipelineAPI import __version__ as VERSION
 
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['maya', 'maya.cmds', 'maya.mel', 
+                'maya.OpenMaya', 'maya.OpenMayaUI',
+                'maya.api.OpenMaya', 'maya.api.OpenMayaUI',
+                'pymel', 'pymel.core', 'pymel.all']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
